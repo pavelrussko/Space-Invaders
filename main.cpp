2b3a7c7ebb5be78bc59ad1647e834b27431b7a99
pavel <pavelrussko@gmail.com>
2024-11-04 20:11:58 +0200

    added window

--- main.cpp
+++ main.cpp
@@ -1,56 +1,75 @@
 #include <cstdio>
+#include <cstdint>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#define GL_ERROR_CASE(glerror)\
-    case glerror: snprintf(error, sizeof(error), "%s", #glerror)
+struct Buffer
+{
+    size_t width, height;
+    uint32_t* data;
+};
 
-void error_callback(int error, const char* description){
-    fprintf(stderr, "Error: %s\n", description);
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s", description);
 }
 
-int main(int argc, char* argv[]){
-    glfwSetErrorCallback(error_callback);
-    GLFWwindow* window;
-
-    if(!glfwInit()){
-        return -1;
+void buffer_clear(Buffer* buffer, uint32_t color)
+{
+    for(size_t i = 0; i < buffer->width * buffer->height; ++i)
+    {
+        buffer->data[i] = color;
     }
+}
+
+uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return (r << 24) | (g << 16) | (b << 8) | 255;
+}
+
+int main(int argc, char* argv[]) {
+    const size_t buffer_width = 224;
+    const size_t buffer_height = 256;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit()) return -1;
 
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    window = glfwCreateWindow(640, 480, "Space Invaders", NULL, NULL);
-    if(!window){
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow *window = glfwCreateWindow(buffer_width, buffer_height, "Space Invaders", NULL, NULL);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    GLenum err = GLEW_OK;
-    if(err != GLEW_OK){
+    GLenum err = glewInit();
+    if(err != GLEW_OK)
+    {
         fprintf(stderr, "Error initializing GLEW.\n");
         glfwTerminate();
         return -1;
     }
-
     int glVersion[2] = {-1, 1};
     glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
     glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]);
 
     printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
+    printf("Renderer used: %s\n", glGetString(GL_RENDERER));
+    printf("Shading Language: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
     glClearColor(1.0, 0.0, 0.0, 1.0);
-    while(!glfwWindowShouldClose(window)){
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+    // Create graphics buffer
+    Buffer buffer;
+    buffer.width  = buffer_width;
+    buffer.height = buffer_height;
+    buffer.data   = new uint32_t[buffer.width * buffer.height];
 
+    buffer_clear(&buffer, 0);
 }
